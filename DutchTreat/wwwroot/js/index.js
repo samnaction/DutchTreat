0c7e0fa53be3e38ec2982e0a1f119639992240b3
Sameer Nazeer <sameernln@gmail.com>
2021-04-24 12:28:49 +0530

    basic html,css,js

---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿console.log("hello dutch treat");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+});
