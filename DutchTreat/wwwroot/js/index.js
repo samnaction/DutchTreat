5fb7f8bc488fcd0531630bb055979dac9f203713
Sameer Nazeer <sameernln@gmail.com>
2021-04-25 08:24:59 +0530

    JQuery Login

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,9 +1,22 @@
-﻿console.log("hello dutch treat");
+﻿$(document).ready(function () {
+    console.log("hello dutch treat");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButton");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on" + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(300);
+    });
 });
